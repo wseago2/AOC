b92c31069d61fa5b06389805324beca0e910f5d3
Wesley Seago <wseago2@fullsail.edu>
2013-05-07 14:24:08 -0400

    Create new project.

---
+++ projectOne/projectOne/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  projectOne
+//
+//  Created by Wesley Seago on 5/7/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
