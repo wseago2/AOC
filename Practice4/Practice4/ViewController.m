8088372fca3fa5fa09d87b7a4cb19df6700e946e
Wesley Seago <wseago2@fullsail.edu>
2013-05-14 13:42:43 -0400

    Add Practice 4.

---
+++ Practice4/Practice4/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Practice4
+//
+//  Created by Wesley Seago on 5/14/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
