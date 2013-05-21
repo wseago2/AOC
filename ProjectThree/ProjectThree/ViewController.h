533e2fc6adf4065a033b85690b704b050ffbf2f7
Wesley Seago <wseago2@fullsail.edu>
2013-05-21 13:42:40 -0400

    Add function.

---
+++ ProjectThree/ProjectThree/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProjectThree
+//
+//  Created by Wesley Seago on 5/21/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
