f0994a61d821a9dc76b9a10bceb03be67865ac6f
Wesley Seago <wseago2@fullsail.edu>
2013-05-28 08:45:34 -0400

    Add Username UILabel.

--- ProjectFour/ProjectFour/ViewController.m
+++ ProjectFour/ProjectFour/ViewController.m
@@ -18,6 +18,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //set background to white
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    //create username UILabel
+    UILabel *username = [[UILabel alloc] initWithFrame:CGRectMake(0,10,100,32)];
+    if (username != nil)
+    {
+        username.backgroundColor = [UIColor whiteColor];
+        username.text = @"Username: ";
+        username.textAlignment = NSTextAlignmentCenter;
+        username.textColor = [UIColor blackColor];
+        [self.view addSubview:username];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
