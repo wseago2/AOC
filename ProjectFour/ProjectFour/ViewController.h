67cf7ec874eaafd4d4db982394491b78afc7051a
Wesley Seago <wseago2@fullsail.edu>
2013-05-28 08:34:54 -0400

    Setup Project Four.

---
+++ ProjectFour/ProjectFour/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProjectFour
+//
+//  Created by Wesley Seago on 5/28/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
